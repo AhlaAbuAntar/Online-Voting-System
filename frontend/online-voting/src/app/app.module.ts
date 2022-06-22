4a8fa62b41b78fc24dcae4d71b800425144e3cfd
Arjanis Kovani <arjanis.kovani@roche.com>
2022-06-22 15:37:02 +0200

    create new project

--- frontend/online-voting/src/app/app.module.ts
+++ frontend/online-voting/src/app/app.module.ts
@@ -3,21 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-
-import { NgxEchartsModule } from 'ngx-echarts';
+import { UmfrageComponent } from './umfrage/umfrage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    UmfrageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxEchartsModule.forRoot({
-      echarts: () => import('echarts')
-    })
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
