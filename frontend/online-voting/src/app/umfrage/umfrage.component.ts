5674018941a58c16765ec2028f5a61e58cdf3936
Arjanis Kovani <arjanis.kovani@roche.com>
2022-06-22 15:37:02 +0200

    create new project

---
+++ frontend/online-voting/src/app/umfrage/umfrage.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-umfrage',
+  templateUrl: './umfrage.component.html',
+  styleUrls: ['./umfrage.component.css']
+})
+
+
+
+export class UmfrageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
