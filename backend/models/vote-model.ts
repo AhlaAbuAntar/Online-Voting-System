d96ec07c9e67bcafd6d89163175bbadd72ffff99
Shamoun Hennawi <shamuonhennawi05@gmail.com>
2022-06-29 14:07:15 +0200

    vote_name added as json object

--- backend/models/vote-model.ts
+++ backend/models/vote-model.ts
@@ -3,11 +3,13 @@ import { Schema, model } from "mongoose";
 
 export interface IVote {
   vote: string;
+  vote_name: string;
   date: Timestamp;
 }
 
 export const voteSchema = new Schema<IVote>({
   vote: { type: String, required: true },
+  vote_name: { type: String, required: true },
   date: { type: Date, default: Date.now },
 });
 
