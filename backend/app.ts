f45dd3afd7d79d4f5b3371f2471e72271aaea397
Shamoun Hennawi <shamuonhennawi05@gmail.com>
2022-06-29 10:51:01 +0200

    help

--- backend/app.ts
+++ backend/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import voteRouter from "./routes/vote";
 
 const app = express();
 dotenv.config();
@@ -14,7 +15,6 @@ app.get("/", (req, res) => {
   res.send("API started").status(200);
 });
 
-import voteRouter from "./routes/vote";
 app.use("/vote", voteRouter);
 
 app.listen(PORT, () => {
